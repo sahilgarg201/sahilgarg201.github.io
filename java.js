586047b2015f87d50e13e524cc50b3f0abe55e76
sahilgarg201 <sahilgarg3468@gmail.com>
2022-06-26 03:05:45 +0530

    updated on 26/06

--- java.js
+++ java.js
@@ -1,3 +1,85 @@
+$(function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $("#header").addClass("active01");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+         $("#header").removeClass("active01");
+      }
+  });
+});
+
+(document).ready(function() {
+  $("#carouselBanner").swiperight(function() {
+    $(this).carousel('prev');
+  });
+  $("#carouselBanner").swipeleft(function() {
+    $(this).carousel('next');
+  });
+});
+    
+
+
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+    ScrollTrigger.create({
+        trigger: elem,
+        start: "top 80%",
+        end: "bottom 20%",
+        markers: true,
+        onEnter: function () {
+            gsap.fromTo(
+                elem,
+                { y: 100, autoAlpha: 0 },
+                {
+                    duration: 1.25,
+                    y: 0,
+                    autoAlpha: 1,
+                    ease: "back",
+                    overwrite: "auto"
+                }
+            );
+        },
+        onLeave: function () {
+            gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        },
+        onEnterBack: function () {
+            gsap.fromTo(
+                elem,
+                { y: -100, autoAlpha: 0 },
+                {
+                    duration: 1.25,
+                    y: 0,
+                    autoAlpha: 1,
+                    ease: "back",
+                    overwrite: "auto"
+                }
+            );
+        },
+        onLeaveBack: function () {
+            gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        }
+    });
+});
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("activate");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+/*
 let answers = document.querySelectorAll(".questions");
 answers.forEach((event) => {
 event.addEventListener("click", () => {
@@ -9,66 +91,25 @@ event.addEventListener("click", () => {
 });
 });
 
-ScrollReveal().reveal('#feature',{ delay: 400 })
-ScrollReveal().reveal('#link',{ delay: 400 })
-ScrollReveal().reveal('.report',{ delay: 400 })
-/*
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 1000) {
-	    $("#header").css("background" , "red");
-	  }
+let valueDisplays = document.querySelectorAll(".num");
+  let interval = 4000;
 
-	  else{
-		  $("#header").css("background" , "white");  	
-	  }
-  })
-})
-*/
+  valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if (startValue == endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  });
 
 
 
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 5) || 900;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
   
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 300 - Math.random() * 500;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
 
 
 /*
